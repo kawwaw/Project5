f833a694dce91fa5fc925ed9c8a30b54dd4d7de8
Frederik Bøgelund Larsen <frederikblarsen@gmail.com>
2018-01-02 20:45:54 +0100

    Added select crime category

--- js/plots.js
+++ js/plots.js
@@ -2,26 +2,23 @@ d3.select(window).on('load', init);
 
 function init() {
 
-    var svgSF = d3.select('#svg2');
-    var wSF = +svgSF.node().getBoundingClientRect().width;
-    var hSF = +svgSF.node().getBoundingClientRect().height;
-
+    var svg = d3.select('#svg2');
+    var wSF = +svg.node().getBoundingClientRect().width;
+    var hSF = +svg.node().getBoundingClientRect().height;
     //Number formatting for population values
     var Mercator = d3.geoMercator()
-        .translate([wSF/2,hSF/2])
-        .scale(150000)
-        .center([-122.447, 37.777]);
+                       .translate([ wSF / 2, hSF / 2 ])
+                       .scale(150000)
+                       .center([ -122.447, 37.777 ]);
 
     console.log("center" + Mercator.center() + " " + Mercator.scale() + " " + Mercator.translate());
 
-
     //create path variable
-    var pathSF =  d3.geoPath()
-        .projection(Mercator);
+    var pathSF = d3.geoPath()
+                     .projection(Mercator);
 
     var offsetMercator = Mercator.translate();
     console.log("offsetM" + offsetMercator);
-
 
     //Define what to do when dragging
     var dragging = function(d) {
@@ -38,93 +35,124 @@ function init() {
 
         //Update projection with new offset
         Mercator.translate(offset);
-        console.log("kontrol" +  Mercator.translate());
 
         //Update all paths and circles
-        svgSF.selectAll("path")
-            .attr("d", pathSF)
-        ;
-        svgSF.selectAll("circle")
-            .attr("cx",function(d){return Mercator([d.geometry.coordinates[0],d.geometry.coordinates[1]])[0]})
-            .attr("cy",function(d){return Mercator([d.geometry.coordinates[0],d.geometry.coordinates[1]])[1]})
-        ;
-
+        svg.selectAll("path")
+            .attr("d", pathSF);
+        svg.selectAll("circle")
+            .attr("cx", function(d) {
+                return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[0]
+            })
+            .attr("cy", function(d) {
+                return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[1]
+            });
     };
 
     //Then define the drag behavior
     var drag = d3.drag()
-        .on("drag", dragging);
+                   .on("drag", dragging);
 
     //Create a container in which all pan-able elements will live
-    var mapSF = svgSF.append("g")
-        .attr("id", "map")
-        .call(drag);  //Bind the dragging behavior
-
+    var mapSF = svg.append("g")
+                    .attr("id", "map");
+    //                   .call(drag); //Bind the dragging behavior
 
     d3.queue()
         .defer(d3.json, 'sf_crime.geojson')
         .defer(d3.json, 'sfpd_districts.geojson')
-        //.defer(d3.json, "https://raw.githubusercontent.com/d3/d3-geo/master/test/data/world-50m.json")
         .await(ready);
 
     function ready(error, crime, json) {
 
-        if (error) throw error;
+        if (error) {
+            throw error;
+        }
 
+        var categories = crime.features
+                             .map(function(a) {
+                                 return a.properties.Category;
+                             })
+                             .filter(function(v, i, s) {
+                                 return s.indexOf(v) === i;
+                             });
 
-        console.log(json);
-        console.log("crime under");
-        console.log(crime);
-        
-        var categories = crime.features.map(function(a){
-          return a.properties.Category;
-        }).filter(function(v, i, s){
-          return s.indexOf(v) === i;
-        });
+        var select = document.getElementById("crime-category");
+        var all = document.createElement("option");
+        all.value = "ALL"
+        all.text = "ALL"
+        select.options.add(all);
+        for (var i = 0; i < categories.length; i++) {
+            var opt = document.createElement("option");
+            opt.value = categories[i];
+            opt.text = categories[i];
+            select.options.add(opt);
+        }
+        select.selectedIndex = 0;
 
-        console.log(categories);
+        var colorCrime = d3.scaleOrdinal().range(d3.schemeCategory10);
+        var data = crime.features;
 
-        var colorCrime = d3.scaleOrdinal()
-            .range(d3.schemeCategory10)
-        ;
-
-        var crimeFea = crime.features;
- //       console.log(crimeFea.properties);
-
-        mapSF.selectAll("path")
+        svg.selectAll("path")
             .data(json.features)
             .enter()
             .append("path")
-            .attr("d",pathSF)
+            .attr("d", pathSF)
             .style("fill", "lightgrey")
             .style("stroke", "blue")
-            .style("stroke-width", 1)
-        ;
+            .style("stroke-width", 1);
 
-        //var colorKat = d3.scaleOrdinal(d3.schemePaired);
-
-        console.log(d3.schemePaired);
-
-        mapSF.selectAll("circle")
-            .data(crime.features)
+        svg
+            .selectAll("circle")
+            .data(data)
             .enter()
             .append("circle")
-            .attr("cx",function(d){return Mercator([d.geometry.coordinates[0],d.geometry.coordinates[1]])[0]})
-            .attr("cy",function(d){return Mercator([d.geometry.coordinates[0],d.geometry.coordinates[1]])[1]})
-            .attr("r",2)
-            //.attr("fill", "green")
-           // .attr("opacity", 0.3)
-            .style("fill", function (d) {
-//                console.log(d.properties.Category);
-                var Kat = d.properties.Category;
-                if (Kat == "LARCENY/THEFT") {
-                    return "red";
-                }
-                if (Kat == "OTHER OFFENSES") {
-                    return "orange";
-                }
-                else {   return "green";}
+            .attr("cx", function(d) {
+                return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[0]
             })
-        ;
+            .attr("cy", function(d) {
+                return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[1]
+            })
+            .attr("r", 2)
+            .attr("fill", "green")
+            .attr("opacity", 0.3);
+
+        d3.select("#crime-category")
+            .on('change', function() {
+                var selectedCategory = select.options[select.selectedIndex].value;
+                if (selectedCategory !== "ALL") {
+                    var dataset = crime.features.filter(function(a) {
+                        return a.properties.Category === selectedCategory;
+                    });
+                    var circle = svg.selectAll("circle").data(dataset);
+                    circle.exit().remove();
+                    circle.enter()
+                        .append("circle")
+                        .attr("cx", function(d) {
+                            return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[0]
+                        })
+                        .attr("cy", function(d) {
+                            return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[1]
+                        })
+                        .attr("r", 2)
+                        .attr("fill", "green")
+                        .attr("opacity", 0.3);
+                } else {
+                    svg
+                        .selectAll("circle")
+                        .data(data)
+                        .enter()
+                        .append("circle")
+                        .attr("cx", function(d) {
+                            return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[0]
+                        })
+                        .attr("cy", function(d) {
+                            return Mercator([ d.geometry.coordinates[0], d.geometry.coordinates[1] ])[1]
+                        })
+                        .attr("r", 2)
+                        .attr("fill", "green")
+                        .attr("opacity", 0.3);
+                }
+
+            });
     }
 }
